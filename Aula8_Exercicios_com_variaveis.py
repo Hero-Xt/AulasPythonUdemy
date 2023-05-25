c3eeed3fdc5af184a0cf23a7dec128b2033abe14
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 12:01:09 -0300

    Create Aula8_Exercicios_com_variaveis.py

---
+++ Aula8_Exercicios_com_variaveis.py
@@ -0,0 +1,13 @@
+nome = str("Gabriel")
+sobrenome = str("Soares")
+idade = int(22)
+ano_nascimento = int(2000)
+maior_idade = bool(idade >= 18)
+altura_metros = float(1.69)
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("È maior de idade:", maior_idade)
+print("Aultura em metros é:",altura_metros)
