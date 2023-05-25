a86f415d012a51e3d964578a04087480081a5538
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 11:49:30 -0300

    Create Aula4_Tipo_int_e_float.py

---
+++ Aula4_Tipo_int_e_float.py
@@ -0,0 +1,21 @@
+# Tipo int e Float
+# int -> Números inteiros
+# int representa qualquer número, negativo ou positivo
+# Int sem sinal é considerado positivo
+print(11) #int
+print(-11) #int
+print(0) #int
+
+# Floar -> Número com ponto flutuante
+# O tipo float representa qualquer número, negativo ou positivo com ponto flutuante
+# Número float sem sinal é considerado número positivo
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+#A função type mostra o tipo que o python inferiu o valor
+print( type("Gabriel") )
+print( type(1) )
+print( type(-10) )
+print( type(0) )
+print( type(-9.0) )
+print( type(0.0) )
