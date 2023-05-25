6c82ca26b2855e672f2e23a4ffb2231fdb85f039
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 11:49:04 -0300

    Create Aula3_Tipo_string.py

---
+++ Aula3_Tipo_string.py
@@ -0,0 +1,23 @@
+"""
+Python = linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que então dentro de aspas
+"""
+print(1234) #por ser uma linguagem dinâmica o python reconhece q se trata de um int
+
+# Aspas simples
+print('Hero-Xt')
+# Aspas dupla
+print("Gabriel")
+
+#Para usar aspas dentro de aspas, basta intercalar elas
+print('Gabriel "Soares"')
+print("Gabriel 'Soares'")
+
+#Codigo um pouco mais "surja"
+# Escape
+print("\"Gabriel\"") #usa a barra invertida como forma de escape
+#Python vai ignorar o caracter apos a \
+# r 
+print(r"\"Gabriel\"") #cai mostras o caracter de escape também
