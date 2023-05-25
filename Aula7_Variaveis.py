93eea1143c43fc0e81dfc4e5e01b66d3d6d56845
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 12:00:41 -0300

    Create Aula7_Variaveis.py

---
+++ Aula7_Variaveis.py
@@ -0,0 +1,16 @@
+# Variáveis são usadas para salvar algo na memória
+# Pep8: inicie vriáveis com letras minúsculas, pode usar números e _
+# O sinal de = é o operador de atribuição. 
+#Ele é usado para atribuir um valor a um nome (variável).
+# Uso: nome_variavel
+nome_completo = 'Gabriel Soares'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo)
+print(soma_dois_mais_dois)
+
+nome = "Luiz"
+idade = 30
+maior_de_idade = idade >= 18
+
+print('Nome: ', nome, "Idade: ", idade)
+print('È maior?: ', maior_de_idade)
