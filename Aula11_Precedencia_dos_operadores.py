e28dce2d67926d2b1da27f7b011250a8649b52f8
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 12:02:14 -0300

    Create Aula11_Precedencia_dos_operadores.py

---
+++ Aula11_Precedencia_dos_operadores.py
@@ -0,0 +1,10 @@
+# 1. (n + n)
+# 2. **
+# 3. *   /   //  %
+# 4. +    -
+# Se tiverem mesmo nível de precedência
+# È excultado por ordem da esquerda para a direito
+conta_1 = 1 + 1 ** 5 + 5
+print(conta_1)
+conta_2 = 1 + (0.5 + 0.5)** (5 + 5)
+print(conta_2)
