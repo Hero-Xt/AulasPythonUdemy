e72bc2e32fa895838d6a7f67dc113f5229206be2
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 12:05:50 -0300

    Create Aula18_Debugger_VS_Code.py

---
+++ Aula18_Debugger_VS_Code.py
@@ -0,0 +1,24 @@
+# if / elif / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True # Verdadeiro para a execução do restante
+condicao4 = True # Não execulta
+
+if condicao1:
+    print("Este é o código1")
+    print("Este é o código1")
+elif condicao2:
+    print("Este é o código2")
+elif condicao3:
+    print("Este é o código3")
+elif condicao4:
+    print("Este é o código4")
+else:
+    print("else")
+
+if 10 == 10:
+    print("outro if")
+
+print("Fora do if")
