d3098c37f31679be2922a6d924e948eea199818d
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 12:03:57 -0300

    Create Aula13_f_strings.py

---
+++ Aula13_f_strings.py
@@ -0,0 +1,7 @@
+nome = "Gabriel Soares"
+altura = 1.69
+peso = 57
+imc = peso / (altura)**2
+# f_strings -> strings formatadas
+print(f"{nome} tem {altura} de altura,") # substitui {} pela variavel
+print(f"pesa {peso} quilos e seu IMC é: {imc:.2f}") #casas decimais 
