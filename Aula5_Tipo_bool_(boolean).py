9ca87767cfddebee722f3634229d1105f7ab5420
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 11:49:56 -0300

    Create Aula5_Tipo_bool_(boolean).py

---
+++ Aula5_Tipo_bool_(boolean).py
@@ -0,0 +1,12 @@
+# Tipo de dado bool, (Boolean)
+# Ao questionar algo no programa
+# Só existe duas respostas possiveis
+# Sim(True), Não(False)
+# Existem varios operadores para questionar
+
+print(10 == 10)
+print(10 == 11)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
