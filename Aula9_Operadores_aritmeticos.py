8c2d9636d14404217da7e4926bfa0c03d3177f73
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-25 12:01:34 -0300

    Create Aula9_Operadores_aritmeticos.py

---
+++ Aula9_Operadores_aritmeticos.py
@@ -0,0 +1,23 @@
+adicao = 2 + 2
+print("Adição:", adicao)
+
+subtracao = 10 - 5
+print("Subtração:", subtracao)
+
+mutiplicacao = 2 * 32
+print("Mutiplicação:" , mutiplicacao)
+
+divisão = 10 / 2.2 #retorno sempre em float
+print("Divisão:", divisão)
+
+divisao_inteira = 10 // 2.2
+print("Divisão inteira:", divisao_inteira)
+
+expornencial = 2 ** 2
+print("Exponencial:", expornencial)
+
+modulo = 25 % 5
+print("Resto:",modulo)
+
+modulo2 = 22 % 5
+print("Resto:",modulo2)
