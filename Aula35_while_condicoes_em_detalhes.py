9f455875f928d93f599902b9922dd041eba08d62
Gabriel Soares <102264526+Hero-Xt@users.noreply.github.com>
2023-05-26 20:54:29 -0300

    Create Aula35_while_condicoes_em_detalhes.py

---
+++ Aula35_while_condicoes_em_detalhes.py
@@ -0,0 +1,11 @@
+"""
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito - > Quanfo um código não tem fim
+"""
+contador = 0
+
+while contador < 10:
+    contador = contador + 1
+    print('Eita')
+
+print('Acabou')
